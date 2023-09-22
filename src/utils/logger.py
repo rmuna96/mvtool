bc2854ee06a6f0aebbe21e6d0a851796f23b25c6
rmuna96 <riccardo.munafo@polimi.it>
2023-09-22 10:52:44 +0200

    update logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,19 +5,17 @@ import os.path as osp
 
 
 class Logger:
-    def __init__(self, odir, fold, name):
-        self.odir = odir
-        self.fold = fold
+    def __init__(self, odir, name):
+        self.log_dir = odir
         self.name = name
 
     def get_logger(self,):
-        log_dir = osp.join(self.odir, f'v{self.fold}')
-        os.makedirs(log_dir, exist_ok=True)
+        os.makedirs(self.log_dir, exist_ok=True)
         logger = logging.getLogger('log')
         logger.setLevel(logging.DEBUG)
-        if osp.exists(osp.join(log_dir, f'{self.name}.log')):
-            os.remove(osp.join(log_dir, f'{self.name}.log'))
-        output_file_handler = logging.FileHandler(osp.join(log_dir, f'{self.name}.log'))
+        if osp.exists(osp.join(self.log_dir, f'{self.name}.log')):
+            os.remove(osp.join(self.log_dir, f'{self.name}.log'))
+        output_file_handler = logging.FileHandler(osp.join(self.log_dir, f'{self.name}.log'))
         logger.addHandler(output_file_handler)
         logger.addHandler(logging.StreamHandler(sys.stdout))
         return logger
