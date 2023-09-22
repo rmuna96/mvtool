ecef29ad27cce57e8d0055be339f97692140b6ad
rmuna96 <riccardo.munafo@polimi.it>
2023-09-22 09:47:48 +0200

    uodate init in utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,11 +1,1 @@
-from utils.functions import (
-    h52vtk,
-    getmodelfromlabelmask,
-    splinefit,
-    soapfilmannulusinterpolation,
-    planefit,
-    clmodeling,
-    anter_postsplit,
-    distancebtwpolydata)
-
 from utils.logger import Logger
